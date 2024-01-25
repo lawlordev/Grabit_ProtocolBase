96630f90d063b6e835f1b02ed255bf234e61c25e
lawlordev <combo_king@icloud.com>
2024-01-25 13:36:07 -0700

    Initial Commit

---
+++ Grabit_ProtocolBase/Grabit_ProtocolBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grabit_ProtocolBaseApp.swift
+//  Grabit_ProtocolBase
+//
+//  Created by Joseph Lawlor on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Grabit_ProtocolBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
